72b6d12b411c3f1d0e16583fb8feeddd2d62f78f
Wp_devsite <leshiy_pos@mail.ru>
2025-04-06 13:34:44 +0300

    "New comit"

--- src/component/header/header.jsx
+++ src/component/header/header.jsx
@@ -3,12 +3,14 @@ import Button from "../button/button";
 import useTelegram from "../../hooks/useTelegram";
 
 export default function Header() {
-  const { user, onClose } = useTelegram();
+  const { tg, user, onClose } = useTelegram();
 
   return (
     <div className={styles.header}>
       <Button onClick={onClose}>Закрыть</Button>
-      <span className={styles.username}>{user?.username}</span>
+      <span className={styles.username}>
+        {tg.initDataUnsafe?.user?.username}
+      </span>
     </div>
   );
 }
